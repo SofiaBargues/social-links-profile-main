89c9ad8eff43f696cfd995c3a5b35aa67e46f0cf
Sofia Bargues <barguessofia@gmail.com>
2024-05-20 11:09:27 +0100

    algo mejor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,24 @@ import jessica from "@/images/avatar-jessica.jpeg";
 export default function Home() {
   return (
     <div className="bg-[#141414] h-screen flex  justify-center  w-screen">
-      <div className="bg-[#1f1f1f] rounded-3xl text-white m-auto flex flex-col h-[767px] w-[482px]">
-        <Image
-          className="rounded-full h-28 w-28 m-auto"
-          alt="jessica"
-          src={jessica}
-        />
-        <div>name</div>
-        <div>location</div>
-        <div>{"Font-end developer and avid reader."}</div>
+      <div className="bg-[#1f1f1f] rounded-3xl text-center text-white m-auto flex flex-col h-[767px] w-[482px]">
+        <div className="flex justify-center">
+          <Image
+            className="rounded-full h-28 w-28 m-14 "
+            alt="jessica"
+            src={jessica}
+          />
+        </div>
+        <div className="font-bold text-3xl">Jessica Randall</div>
+        <div className="text-[#bfde5c] text-lg font-bold">
+          London, United Kingdom
+        </div>
+        <div className=" font-extralight">
+          "Font-end developer and avid reader."
+        </div>
+        <div className="bg-[#333333] rounded-md flex justify-center h-16 w-36">
+          <div>gdrf</div>
+        </div>
       </div>
     </div>
   );
