5b1b1879e609d0afa6d8252604f05b031e887140
Sofia Bargues <barguessofia@gmail.com>
2024-07-23 15:08:27 +0100

    links

--- src/app/button.tsx
+++ src/app/button.tsx
@@ -1,7 +1,10 @@
 export function Button({ name }: { name: string }) {
   return (
-    <div className="bg-[#333333] text-white hover:bg-[#b9ed32] cursor-pointer hover:text-[#333333] font-bold  rounded-lg text-lg flex flex-col align-middle justify-center h-14 w-full">
+    <a
+      href="https://github.com/SofiaBargues"
+      className="bg-[#333333] text-white hover:bg-[#b9ed32] cursor-pointer hover:text-[#333333] font-bold  rounded-lg text-lg flex flex-col align-middle justify-center h-14 w-full"
+    >
       <div>{name}</div>
-    </div>
+    </a>
   );
 }
